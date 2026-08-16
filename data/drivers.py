46873090313d7cfb62b9d20d6e66df84aa30092d
RedRaceTeam <katrinkuzua12@gmail.com>
2026-08-17 02:00:16 +0700

    Update drivers.py

--- data/drivers.py
+++ data/drivers.py
@@ -1,14 +1,14 @@
 # data/drivers.py
-# Все 33 пилота IndyCar 2026
+# 33 пилота IndyCar 2026
 
 DRIVERS = {
-    # ===== CHIP GANASSI RACING =====
+    # ===== CHIP GANASSI RACING (Honda) =====
     "PAL": {
         "name": "Alex Palou",
         "team": "Chip Ganassi Racing",
         "number": 10,
         "rarity": "ULTIMATE",
-        "price": 1200,
+        "price": 5500,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/AlexPalou.png"
     },
     "DIX": {
@@ -16,33 +16,33 @@ DRIVERS = {
         "team": "Chip Ganassi Racing",
         "number": 9,
         "rarity": "ULTIMATE",
-        "price": 1000,
+        "price": 4500,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/ScottDixon.png"
     },
     "SIM": {
         "name": "Kyffin Simpson",
         "team": "Chip Ganassi Racing",
         "number": 8,
-        "rarity": "REGULAR",
-        "price": 80,
+        "rarity": "COMMON",
+        "price": 100,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/KyffinSimpson.png"
     },
     
-    # ===== TEAM PENSKE =====
+    # ===== TEAM PENSKE (Chevrolet) =====
     "NEW": {
         "name": "Josef Newgarden",
         "team": "Team Penske",
         "number": 2,
         "rarity": "LEGENDARY",
-        "price": 850,
+        "price": 3200,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/JosefNewgarden.png"
     },
     "MCL": {
         "name": "Scott McLaughlin",
         "team": "Team Penske",
         "number": 3,
-        "rarity": "EXCLUSIVE",
-        "price": 520,
+        "rarity": "EPIC",
+        "price": 1500,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/ScottMcLaughlin.png"
     },
     "MAL": {
@@ -50,17 +50,17 @@ DRIVERS = {
         "team": "Team Penske",
         "number": 12,
         "rarity": "LEGENDARY",
-        "price": 700,
+        "price": 2800,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/DavidMalukas.png"
     },
     
-    # ===== ANDRETTI GLOBAL =====
+    # ===== ANDRETTI GLOBAL (Honda) =====
     "KIR": {
         "name": "Kyle Kirkwood",
         "team": "Andretti Global",
         "number": 27,
         "rarity": "LEGENDARY",
-        "price": 800,
+        "price": 2500,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/KyleKirkwood.png"
     },
     "ERI": {
@@ -68,7 +68,7 @@ DRIVERS = {
         "team": "Andretti Global",
         "number": 28,
         "rarity": "RARE",
-        "price": 380,
+        "price": 900,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/MarcusEricsson.png"
     },
     "POW": {
@@ -76,51 +76,51 @@ DRIVERS = {
         "team": "Andretti Global",
         "number": 26,
         "rarity": "ULTIMATE",
-        "price": 950,
+        "price": 4000,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/WillPower.png"
     },
     
-    # ===== ARROW MCLAREN =====
+    # ===== ARROW MCLAREN (Chevrolet) =====
     "OWA": {
         "name": "Pato O'Ward",
         "team": "Arrow McLaren",
         "number": 5,
         "rarity": "LEGENDARY",
-        "price": 750,
+        "price": 3000,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/PatoOWard.png"
     },
     "LUN": {
         "name": "Christian Lundgaard",
         "team": "Arrow McLaren",
         "number": 7,
-        "rarity": "EXCLUSIVE",
-        "price": 600,
+        "rarity": "EPIC",
+        "price": 1600,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/ChristianLundgaard.png"
     },
     "SIE": {
         "name": "Nolan Siegel",
         "team": "Arrow McLaren",
         "number": 6,
-        "rarity": "REGULAR",
-        "price": 100,
+        "rarity": "COMMON",
+        "price": 120,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/NolanSiegel.png"
     },
     "HUN": {
         "name": "Ryan Hunter-Reay",
         "team": "Arrow McLaren",
         "number": 31,
-        "rarity": "EXCLUSIVE",
-        "price": 450,
+        "rarity": "EPIC",
+        "price": 1400,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/RyanHunterReay.png"
     },
     
-    # ===== MEYER SHANK RACING =====
+    # ===== MEYER SHANK RACING (Honda) =====
     "ROS": {
         "name": "Felix Rosenqvist",
         "team": "Meyer Shank Racing",
         "number": 60,
-        "rarity": "EXCLUSIVE",
-        "price": 550,
+        "rarity": "EPIC",
+        "price": 1600,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/FelixRosenqvist.png"
     },
     "ARM": {
@@ -128,7 +128,7 @@ DRIVERS = {
         "team": "Meyer Shank Racing",
         "number": 66,
         "rarity": "RARE",
-        "price": 300,
+        "price": 600,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/MarcusArmstrong.png"
     },
     "CAS": {
@@ -136,33 +136,33 @@ DRIVERS = {
         "team": "Meyer Shank Racing",
         "number": 6,
         "rarity": "INDY_EDITION",
-        "price": 1000,
+        "price": 3500,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/HelioCastroneves.png"
     },
     
-    # ===== RAHAL LETTERMAN LANIGAN =====
+    # ===== RAHAL LETTERMAN LANIGAN (Honda) =====
     "RAH": {
         "name": "Graham Rahal",
         "team": "Rahal Letterman Lanigan Racing",
         "number": 15,
         "rarity": "RARE",
-        "price": 250,
+        "price": 500,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/GrahamRahal.png"
     },
     "FOS": {
         "name": "Louis Foster",
         "team": "Rahal Letterman Lanigan Racing",
         "number": 45,
-        "rarity": "REGULAR",
-        "price": 120,
+        "rarity": "COMMON",
+        "price": 100,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/LouisFoster.png"
     },
     "SCH": {
         "name": "Mick Schumacher",
         "team": "Rahal Letterman Lanigan Racing",
         "number": 47,
-        "rarity": "EXCLUSIVE",
-        "price": 450,
+        "rarity": "EPIC",
+        "price": 2000,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/MickSchumacher.png"
     },
     "SAT": {
@@ -170,25 +170,25 @@ DRIVERS = {
         "team": "Rahal Letterman Lanigan Racing",
         "number": 75,
         "rarity": "INDY_EDITION",
-        "price": 600,
+        "price": 2500,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/TakumaSato.png"
     },
     
-    # ===== ED CARPENTER RACING =====
+    # ===== ED CARPENTER RACING (Chevrolet) =====
     "ROSS": {
         "name": "Alexander Rossi",
         "team": "Ed Carpenter Racing",
         "number": 20,
-        "rarity": "EXCLUSIVE",
-        "price": 400,
+        "rarity": "EPIC",
+        "price": 1400,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/AlexanderRossi.png"
     },
     "RAS": {
         "name": "Christian Rasmussen",
         "team": "Ed Carpenter Racing",
         "number": 21,
-        "rarity": "REGULAR",
-        "price": 90,
+        "rarity": "COMMON",
+        "price": 100,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/ChristianRasmussen.png"
     },
     "CAR": {
@@ -196,71 +196,71 @@ DRIVERS = {
         "team": "Ed Carpenter Racing",
         "number": 33,
         "rarity": "RARE",
-        "price": 200,
+        "price": 400,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/EdCarpenter.png"
     },
     
-    # ===== A.J. FOYT ENTERPRISES =====
+    # ===== A.J. FOYT ENTERPRISES (Chevrolet) =====
     "FER": {
         "name": "Santino Ferrucci",
         "team": "A.J. Foyt Enterprises",
         "number": 14,
         "rarity": "RARE",
-        "price": 180,
+        "price": 450,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/SantinoFerrucci.png"
     },
     "COL": {
         "name": "Caio Collet",
         "team": "A.J. Foyt Enterprises",
         "number": 4,
-        "rarity": "REGULAR",
+        "rarity": "COMMON",
         "price": 100,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/CaioCollet.png"
     },
     
-    # ===== JUNCOS HOLLINGER RACING =====
+    # ===== JUNCOS HOLLINGER RACING (Chevrolet) =====
     "VEE": {
         "name": "Rinus VeeKay",
         "team": "Juncos Hollinger Racing",
         "number": 76,
         "rarity": "RARE",
-        "price": 350,
+        "price": 700,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/RinusVeeKay.png"
     },
     "ROB": {
         "name": "Sting Ray Robb",
         "team": "Juncos Hollinger Racing",
         "number": 77,
-        "rarity": "REGULAR",
-        "price": 80,
+        "rarity": "COMMON",
+        "price": 100,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/StingRayRobb.png"
     },
     
-    # ===== DALE COYNE RACING =====
+    # ===== DALE COYNE RACING (Honda) =====
     "GRO": {
         "name": "Romain Grosjean",
         "team": "Dale Coyne Racing",
         "number": 18,
         "rarity": "RARE",
-        "price": 280,
+        "price": 650,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/RomainGrosjean.png"
     },
     "HAU": {
         "name": "Dennis Hauger",
         "team": "Dale Coyne Racing",
         "number": 19,
-        "rarity": "REGULAR",
-        "price": 120,
+        "rarity": "COMMON",
+        "price": 100,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/DennisHauger.png"
     },
     
-    # ===== PREMA RACING =====
+    # ===== PREMA RACING (Chevrolet) =====
     "ILO": {
         "name": "Callum Ilott",
         "team": "PREMA Racing",
         "number": 90,
         "rarity": "RARE",
-        "price": 200,
+        "price": 500,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/CallumIlott.png"
     },
     "SHW": {
@@ -268,45 +268,45 @@ DRIVERS = {
         "team": "PREMA Racing",
         "number": 83,
         "rarity": "RARE",
-        "price": 180,
+        "price": 500,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/RobertShwartzman.png"
     },
     
-    # ===== DREYER & REINBOLD =====
+    # ===== DREYER & REINBOLD (Chevrolet) =====
     "DAL": {
         "name": "Conor Daly",
         "team": "Dreyer & Reinbold Racing",
         "number": 23,
         "rarity": "RARE",
-        "price": 220,
+        "price": 400,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/ConorDaly.png"
     },
     "HAR": {
         "name": "Jack Harvey",
         "team": "Dreyer & Reinbold Racing",
         "number": 24,
-        "rarity": "REGULAR",
+        "rarity": "COMMON",
         "price": 150,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/JackHarvey.png"
     },
     
-    # ===== HMD MOTORSPORTS =====
+    # ===== HMD MOTORSPORTS (Chevrolet) =====
     "LEG": {
         "name": "Katherine Legge",
         "team": "HMD Motorsports",
         "number": 11,
-        "rarity": "REGULAR",
-        "price": 80,
+        "rarity": "COMMON",
+        "price": 100,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/KatherineLegge.png"
     },
     
-    # ===== ABEL MOTORSPORTS =====
+    # ===== ABEL MOTORSPORTS (Chevrolet) =====
     "ABE": {
         "name": "Jacob Abel",
         "team": "Abel Motorsports",
         "number": 51,
-        "rarity": "REGULAR",
-        "price": 90,
+        "rarity": "COMMON",
+        "price": 100,
         "image": "https://www.indycar.com/-/media/IndyCar/Drivers/IndyCar-Series/Driver-List/JacobAbel.png"
     }
 }
