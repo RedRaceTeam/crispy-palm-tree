c5c922fcbb53e818731ccff553a38a13c8eb9d9e
RedRaceTeam <katrinkuzua12@gmail.com>
2026-08-17 04:27:49 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+WEBHOOK_PATH = "/webhook"
+PORT = int(os.getenv("PORT", 8000))
+ADMIN_IDS = [7025868617, 7946032603]
+DB_PATH = "indycard.db"
